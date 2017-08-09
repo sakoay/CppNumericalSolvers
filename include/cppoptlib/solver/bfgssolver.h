c01f696f2e2a64493d881055ce61d183dfe472e2
Sue Ann Koay <koay@princeton.edu>
2017-08-09 11:09:22 -0400

    More printouts and tunable x tolerance

--- include/cppoptlib/solver/bfgssolver.h
+++ include/cppoptlib/solver/bfgssolver.h
@@ -23,7 +23,15 @@ class BfgsSolver : public ISolver<ProblemType, 1> {
         TVector grad(DIM);
         TVector x_old = x0;
         this->m_current.reset();
+#ifdef SAK_DEBUG_PRINT
+        Scalar f_old = objFunc.valueAndGradient(x0, grad);
+#else
         objFunc.gradient(x0, grad);
+#endif
+        if (this->m_stop.xDelta <= 0)
+          this->m_stop.xDelta = 1e-7;      // SAK tunable criteria
+
+
         do {
             TVector searchDir = -1 * H * grad;
             // check "positive definite"
@@ -40,7 +48,11 @@ class BfgsSolver : public ISolver<ProblemType, 1> {
             x0 = x0 + rate * searchDir;
 
             TVector grad_old = grad;
+#ifdef SAK_DEBUG_PRINT
+            Scalar f = objFunc.valueAndGradient(x0, grad);
+#else
             objFunc.gradient(x0, grad);
+#endif
             TVector s = rate * searchDir;
             TVector y = grad - grad_old;
 
@@ -48,9 +60,16 @@ class BfgsSolver : public ISolver<ProblemType, 1> {
             H = H - rho * (s * (y.transpose() * H) + (H * y) * s.transpose()) + rho * rho * (y.dot(H * y) + 1.0 / rho)
                 * (s * s.transpose());
             // std::cout << "iter: "<<iter<< " f = " <<  objFunc.value(x0) << " ||g||_inf "<<gradNorm   << std::endl;
+#ifdef SAK_DEBUG_PRINT
+            mexPrintf(" [%5d]  f = %10.5g (%10.5g), ||g|| = %10.5g, x = [", this->m_current.iterations, f, f_old - f, grad.norm());
+            for (Eigen::Index iX = 0; iX < x0.size(); ++iX)
+              mexPrintf("%s %8.3g", iX ? "," : "", x0[iX]);
+            mexPrintf(" ]\n");
+            mexEvalString("drawnow");
+            f_old = f;
+#endif
 
-            if( (x_old-x0).template lpNorm<Eigen::Infinity>() < 1e-7  )
-                break;
+            this->m_current.xDelta = (x_old-x0).template lpNorm<Eigen::Infinity>();
             x_old = x0;
             ++this->m_current.iterations;
             this->m_current.gradNorm = grad.template lpNorm<Eigen::Infinity>();
