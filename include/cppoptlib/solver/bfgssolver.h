60bb08f1ddd51f4b4fa29c611c9ee9ad94ba6618
Sue Ann Koay <koay@princeton.edu>
2017-08-20 17:48:16 -0400

    Avoid rho*rho / rho

--- include/cppoptlib/solver/bfgssolver.h
+++ include/cppoptlib/solver/bfgssolver.h
@@ -57,7 +57,7 @@ class BfgsSolver : public ISolver<ProblemType, 1> {
             TVector y = grad - grad_old;
 
             const Scalar rho = 1.0 / y.dot(s);
-            H = H - rho * (s * (y.transpose() * H) + (H * y) * s.transpose()) + rho * rho * (y.dot(H * y) + 1.0 / rho)
+            H = H - rho * (s * (y.transpose() * H) + (H * y) * s.transpose()) + rho * (rho * y.dot(H * y) + 1.0)
                 * (s * s.transpose());
             // std::cout << "iter: "<<iter<< " f = " <<  objFunc.value(x0) << " ||g||_inf "<<gradNorm   << std::endl;
 #ifdef SAK_DEBUG_PRINT
